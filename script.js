0088bd5b815679f1d686b0d8375aa6b871bc6416
Kenneth <obikenneth913@gmail.com>
2022-05-03 11:24:56 +0300

    building promises

--- script.js
+++ script.js
@@ -161,7 +161,7 @@ const whereAmI = (lat, lng) => {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
     .then(response => {
       if (!response.ok)
-        throw new Error(`Location could not be found ${response.status}`);
+        throw new Error(`Problem with geocoding ${response.status}`);
       return response.json();
     })
     .then(data => {
@@ -170,8 +170,7 @@ const whereAmI = (lat, lng) => {
       return fetch(`https://restcountries.com/v2/name/${data.country}`);
     })
     .then(response => {
-      if (!response.ok)
-        throw new Error(`Location could not be found ${response.status}`);
+      if (!response.ok) throw new Error(`Country not found ${response.status}`);
       return response.json();
     })
     .then(([data]) => {
@@ -183,3 +182,42 @@ const whereAmI = (lat, lng) => {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You win');
+    } else {
+      reject(new Error('You lost your money'));
+    }
+  }, 2000);
+});
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+//Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(2);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('4 second passed');
+    return wait(1);
+  });
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem')).catch(x => console.log(x));
